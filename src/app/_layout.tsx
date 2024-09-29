efedb66b6654ea4772fbee4f6d2c6e3f7a0cc1f7
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-09-28 22:49:14 -0300

    chore: add tailwindcss and poppins font  and component loading

---
+++ src/app/_layout.tsx
@@ -0,0 +1,16 @@
+import '../styles/global.css'
+import {Slot} from "expo-router";
+import {useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold} from '@expo-google-fonts/poppins' 
+import { Loading } from '../components/loading';
+
+export default function LayoutRoot(){
+  const [fontLoaded] = useFonts({Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold})
+
+  if (!fontLoaded) {
+    return <Loading/>
+  }
+
+  return (
+    <Slot/>
+  )
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home(){
+  return (
+    <View className="flex-1 items-center bg-green-300 font-medium">
+      <Text className="text-green-900">hello word</Text>
+    </View>
+  )
+}
