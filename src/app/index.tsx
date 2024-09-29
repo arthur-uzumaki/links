eb67e9f373da96dbd6263bdad06e299dc955b33e
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-09-28 22:55:06 -0300

    chore: setup biome

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
 
-export default function Home(){
+export default function Home() {
   return (
     <View className="flex-1 items-center bg-green-300 font-medium">
       <Text className="text-green-900">hello word</Text>
